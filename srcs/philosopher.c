b1ac86e48c36f124198e673b4bc638fc620cb57b
dantremb <dany.tremblay@gmail.com>
2022-07-20 23:53:46 -0400

    20 July 23:53:46

--- srcs/philosopher.c
+++ srcs/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: dantremb <dantremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 23:48:30 by dantremb          #+#    #+#             */
-/*   Updated: 2022/07/20 20:08:17 by dantremb         ###   ########.fr       */
+/*   Updated: 2022/07/20 23:46:11 by dantremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ unsigned long	ft_get_ms(t_philo *philo)
 	return (ft_get_time() - philo->table->time);
 }
 
-/*
-pthread_mutex_t	*ft_init_fork(t_philo *philo, t_table *table)
+
+pthread_mutex_t	*ft_init_fork(t_table *table)
 {
 	pthread_mutex_t	*fork;
 	int				i;
@@ -41,16 +41,17 @@ pthread_mutex_t	*ft_init_fork(t_philo *philo, t_table *table)
 	fork = malloc(sizeof(pthread_mutex_t) * table->philo_count);
 	while (++i < table->philo_count)
 		if (pthread_mutex_init(&fork[i], NULL) != 0)
-			return (1);
+			return (NULL);
+	return (fork);
 }
-*/
+
 t_philo	*ft_init_philo(t_table *table)
 {
 	t_philo			*philo;
-	//pthread_mutex_t	*fork;
+	pthread_mutex_t	*fork;
 	int				i;
 	
-	//fork = ft_init_fork(philo, table);
+	fork = ft_init_fork(table);
 	philo = ft_calloc(sizeof(t_philo), table->philo_count + 1);
 	i = -1;
 	while(++i < table->philo_count)
@@ -59,6 +60,7 @@ t_philo	*ft_init_philo(t_table *table)
 		philo[i].table = table;
 		philo[i].eated_meal = 0;
 		philo[i].last_meal = 0;
+		philo[i].fork = fork;
 	}
 	return (philo);
 }
@@ -78,6 +80,18 @@ t_philo	*ft_init_table(t_table *table, int argc, char **argv)
 	return (ft_init_philo(table));
 }
 
+void	ft_drop_fork(t_philo *philo, int fork1, int fork2)
+{
+	pthread_mutex_unlock(&philo->fork[fork1]);
+	pthread_mutex_unlock(&philo->fork[fork2]);
+}
+
+void	ft_take_a_fork(t_philo *philo, int fork)
+{
+	pthread_mutex_lock(&philo->fork[fork]);
+	printf("%lums %d has taken a fork\n",ft_get_ms(philo), philo->name);
+}
+
 void	*ft_eating(void *arg)
 {
 	t_philo	*philo;
@@ -88,13 +102,16 @@ void	*ft_eating(void *arg)
 	philo->last_meal = ft_get_time();
 	while (1)
 	{
-		printf("%lums %d has taken a fork\n",ft_get_ms(philo), philo->name);
+		ft_take_a_fork(philo, philo->name - 1);
+		ft_take_a_fork(philo, philo->name % philo->table->philo_count);
 		printf("%lums %d is eating!\n",ft_get_ms(philo), philo->name);
 		usleep(philo->table->time_to_eat * 1000);
+		philo->last_meal = ft_get_time();
+		ft_drop_fork(philo, philo->name - 1, philo->name % philo->table->philo_count);
+		printf("%lums %d is sleeping\n",ft_get_ms(philo), philo->name);
 		philo->eated_meal += 1;
 		if (philo->eated_meal == philo->table->meal_count)
 			philo->table->finished++;
-		printf("%lums %d is sleeping\n",ft_get_ms(philo), philo->name);
 		usleep(philo->table->time_to_sleep * 1000);
 		printf("%lums %d is thinking\n",ft_get_ms(philo), philo->name);
 	}
@@ -103,11 +120,19 @@ void	*ft_eating(void *arg)
 
 void	ft_death_watcher(t_table *table, t_philo *philo)
 {
-	(void)philo;
+	int	i;
+
+	i = 0;
 	while (1)
 	{
 		if (table->finished == table->philo_count)
 			exit(0);
+		if (ft_get_time() - philo[i].last_meal > (unsigned long)table->time_to_die)
+		{
+			printf("%lums %d is dead\n",ft_get_ms(&philo[i]), philo[i].name);
+			exit(1);
+		}
+		i = (i + 1) % table->philo_count;
 		usleep(1000);
 	}
 }
@@ -141,15 +166,3 @@ int	main(int argc, char **argv)
 	ft_sit_at_table(&table, philo);
 	return (0);
 }
-
-//	printf("Test from threads no\n");
-//	while (i < 10)
-//	{
-//	pthread_mutex_lock(&r->mutex);
-//		r->add++;
-//		i++;
-//		printf("adding loop = %d\n", r->add);
-//	pthread_mutex_unlock(&r->mutex);
-//	usleep(1);
-//	}
-//	printf("Ending thread with add = %d\n", r->add);
